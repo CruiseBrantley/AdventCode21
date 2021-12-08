b72be5068cd042b2ded6d861e5342d85996f1e3c
Cruise Brantley <cruise.brantley@gmail.com>
2021-12-07 22:48:54 -0600

    Part 2 complete

--- 3/index.js
+++ 3/index.js
@@ -9,13 +9,35 @@ function mostCommonElement(array) {
     return obj['0'] > obj['1'] ? '0' : '1'
 }
 
-function partOne(data) {
+function removeWrongValues(arr) {
+    const ratingsObj = {}
+    const ratings = (arr, mostCommon) => {
+        let lengthArr = createLengthArray(arr)
+        for(let i = 0; i < lengthArr.length; i++) {
+            if (arr.length <= 1) break
+            const commonElement = mostCommonElement(lengthArr[i])
+            arr = arr.filter(arrElement => mostCommon ? arrElement[i] === commonElement : arrElement[i] !== commonElement )
+            lengthArr = createLengthArray(arr)
+        }
+        return arr[0]
+    }
+    ratingsObj['oxygenRating'] = ratings(arr, true)
+    ratingsObj['co2Rating'] = ratings(arr, false)
+    return ratingsObj
+}
+
+function createLengthArray(data) {
     const lengthArr = Array.from(new Array(data[0].length), () => [])
-    for(let i = 0; i < data[0].length; i++) {
+    for(let i = 0; i < data[0].length ?? 0; i++) {
         for(let j = 0; j < data.length; j++) {
             lengthArr[i][j] = data[j][i]
         }
     } // create array length wise from strings
+    return lengthArr
+}
+
+function partOne(data) {
+    const lengthArr = createLengthArray(data)
     const mostCommonArr = lengthArr.map(line => mostCommonElement(line))
     const gamma = parseInt(mostCommonArr.join(''), 2)
     const epsilon = parseInt(mostCommonArr.map(element => element === '1' ? '0' : '1').join(''), 2)
@@ -23,7 +45,13 @@ function partOne(data) {
 }
 
 function partTwo(data) {
-    
+    const lengthArr = createLengthArray(data)
+    const correctValues = removeWrongValues(data, lengthArr)
+    parseInt(correctValues['oxygenRating'].join(''), 2)
+    parseInt(correctValues['co2Rating'].join(''), 2)
+    const oxygenRating = parseInt(correctValues['oxygenRating'].join(''), 2)
+    const co2Rating = parseInt(correctValues['co2Rating'].join(''), 2)
+    return oxygenRating * co2Rating
 }
 
 console.log(`Part One: ${partOne(charArray)}`)
